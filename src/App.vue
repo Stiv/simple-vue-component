cc5fa3c06cf97cca56181bf339af45899c20faef
sbruno <stefano.bruno@bitrock.it>
2018-05-25 13:18:51 +0200

    add export for component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Component msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Component from '../index'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Component
   }
 }
 </script>
