f5b0fb0efc54272b8dc6f0a1b8435c185b772d26
Stefano <sb@stivnet.net>
2018-05-26 10:40:41 +0200

    test 1

--- src/component.vue
+++ src/component.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-    Hello
+    <ag-grid-vue style="width: 500px; height: 500px;"
+                 class="ag-theme-balham"
+                 :gridOptions="gridOptions"
+                 :columnDefs="columnDefs"
+                 :rowData="rowData">
+    </ag-grid-vue>
   </div>
 </template>
 
 <script>
+import "../node_modules/ag-grid/dist/styles/ag-grid.css";
+import "../node_modules/ag-grid/dist/styles/ag-theme-balham.css";
+
 export default {
   name: 'SimpleComponent',
-  props: {
-    msg: String
-  }
+    data() {
+        return {
+            gridOptions: {},
+            columnDefs: null,
+            rowData: null
+        }
+    },
+    components: {
+        'ag-grid-vue': AgGridVue
+    },
+    beforeMount() {
+        this.columnDefs = [
+            {headerName: 'Make', field: 'make'},
+            {headerName: 'Model', field: 'model'},
+            {headerName: 'Price', field: 'price'}
+        ];
+
+        this.rowData = [
+            {make: 'Toyota', model: 'Celica', price: 35000},
+            {make: 'Ford', model: 'Mondeo', price: 32000},
+            {make: 'Porsche', model: 'Boxter', price: 72000}
+        ];
+    }
 }
 </script>
 
